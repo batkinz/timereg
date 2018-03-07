7c33e6faa057247c7d1ad13f92eb32648792e56b
Bence Makkos <bence.makkos@gmail.com>
2018-03-07 18:39:38 +0100

    moving pages to new "pages" folder

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
-import { RegisterTime } from './components/RegisterTime';
-import { CreateInvoice } from './components/CreateInvoice';
+import { RegisterTime } from './pages/RegisterTime';
+import { CreateInvoice } from './pages/CreateInvoice';
 
 export const routes = <Layout>
     <Route exact path='/' component={ RegisterTime } />
